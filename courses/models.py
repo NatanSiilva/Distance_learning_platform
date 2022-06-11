e163dd12b16137288d11d1ab989b4cf5cac48c6a
Natanael <natanaelsilva.progjs@gmail.com>
2022-06-11 11:58:45 -0300

    feat: relations generic with contentType

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.auth.models import User
 
 
@@ -38,3 +40,14 @@ class Module(models.Model):
 
     def __str__(self):
         return '{}. {}'.format(self.order, self.title)
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
+    content_type = models.Foreignkey(
+        ContentType,
+        on_delete=models.CASCADE,
+    )
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
