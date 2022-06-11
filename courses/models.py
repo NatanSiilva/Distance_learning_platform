ece79d9e5f6eeb9382ccdc1ad652a57b85141657
Natanael <natanaelsilva.progjs@gmail.com>
2022-06-11 13:57:09 -0300

    feat: criation custom field OrderField

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.auth.models import User
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -34,6 +35,7 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
 
     class meta:
         ordering = ['order']
@@ -44,22 +46,26 @@ class Module(models.Model):
 
 class Content(models.Model):
     module = models.ForeignKey(Module, related_name='contents', on_delete=models.CASCADE)
-    content_type = models.Foreignkey(
+    content_type = models.ForeignKey(
         ContentType,
         on_delete=models.CASCADE,
         limit_choices_to={'model__in': ('text', 'video', 'image', 'file')}
     )
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
     owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    class meta:
+    class Meta:
         abstract = True
 
     def __str__(self):
