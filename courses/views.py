5a23cffa85abede01a639c755cd27f042bffb15a
Natanael <natanaelsilva.progjs@gmail.com>
2022-06-17 20:02:44 -0300

    feat: add view ordination module and content in course

--- courses/views.py
+++ courses/views.py
@@ -6,6 +6,8 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.forms.models import modelform_factory
 from django.views.generic.base import TemplateResponseMixin, View
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+
 
 from .models import Course, Module, Content
 from .forms import ModuleFormSet
@@ -141,3 +143,17 @@ class ModuleContentListView(TemplateResponseMixin, View):
     def get(self, request, module_id):
         module = get_object_or_404(Module, id=module_id, course__owner=request.user)
         return self.render_to_response({'module': module})
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.item():
+            Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self, request):
+        for id, order in self.request_json.item():
+            Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
