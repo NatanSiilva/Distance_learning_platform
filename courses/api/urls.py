cc0555c481c43c1c96b585a314f7283e20d9c5a2
Natanael <natanaelsilva.progjs@gmail.com>
2022-06-21 14:54:07 -0300

    feat: urls add api

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 app_name = 'courses'
 
-router = routers.DefaultRouter()
-router.register('courses', views.CourseViewSet)
-
 urlpatterns = [
      path('subjects/', views.SubjectListView.as_view(), name='subject_list'),
      path('subjects/<pk>/', views.SubjectDetailView.as_view(), name='subject_detail'),
